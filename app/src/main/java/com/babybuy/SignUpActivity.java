a1e38118dc7b668616404aafb33bb2d40fa8924a
Nirajan Kunwor <nirajankunwor604@gmail.com>
2022-12-07 12:23:31 +0545

    feat: sign up details added to real time db

--- app/src/main/java/com/babybuy/SignUpActivity.java
+++ app/src/main/java/com/babybuy/SignUpActivity.java
@@ -13,36 +13,58 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
     private FirebaseAuth auth;
-    private EditText signupEmail, signupPassword;
+    private EditText signupName, signupUsername, signupEmail, signupPassword;
     private Button signupButton;
     private TextView loginRedirectText;
+
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         auth = FirebaseAuth.getInstance();
+
+        signupName = findViewById(R.id.signup_name);
         signupEmail = findViewById(R.id.signup_email);
+        signupUsername = findViewById(R.id.signup_username);
         signupPassword = findViewById(R.id.signup_password);
         signupButton = findViewById(R.id.signup_button);
         loginRedirectText = findViewById(R.id.loginRedirectText);
+
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String user = signupEmail.getText().toString().trim();
-                String pass = signupPassword.getText().toString().trim();
-                if (user.isEmpty()){
+
+                database = FirebaseDatabase.getInstance();
+                reference = database.getReference("users");
+
+                String name = signupName.getText().toString();
+                String email = signupEmail.getText().toString();
+                String username = signupUsername.getText().toString();
+                String password = signupPassword.getText().toString();
+
+                if (email.isEmpty()){
                     signupEmail.setError("Email cannot be empty");
-                }
-                if (pass.isEmpty()){
+                } else if (password.isEmpty()){
                     signupPassword.setError("Password cannot be empty");
+                } else if (username.isEmpty()){
+                    signupUsername.setError("Username cannot be empty");
+                } else if (name.isEmpty()){
+                    signupName.setError("Name cannot be empty");
                 } else{
-                    auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
+                                User user = new User(name, email, username, password);
+
+                                reference.child(username).setValue(user);
                                 Toast.makeText(SignUpActivity.this, "SignUp Successful", Toast.LENGTH_SHORT).show();
                                 startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
                             } else {
